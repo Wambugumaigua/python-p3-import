a20e3d59fb9ccb31e5f6edb6a3de5f85ee9225b4
alex.wambugu@student.moringaschool.com <wambugumaigua@DESKTOP-F32N3MJ>
2024-06-21 10:31:29 -0700

    complete

--- lib/testing/conftest.py
+++ lib/testing/conftest.py
@@ -1,9 +1,23 @@
-#!/usr/bin/env python3
-
 def pytest_itemcollected(item):
-    par = item.parent.obj
-    node = item.obj
-    pref = par.__doc__.strip() if par.__doc__ else par.__class__.__name__
-    suf = node.__doc__.strip() if node.__doc__ else node.__name__
-    if pref or suf:
-        item._nodeid = ' '.join((pref, suf))
+    parent = item.parent.obj  
+    test_func = item.obj  
+    
+    
+    if parent.__doc__:
+        prefix = parent.__doc__.strip()
+    else:
+        prefix = parent.__class__.__name__
+    
+    
+    if test_func.__doc__:
+        suffix = test_func.__doc__.strip()
+    else:
+        suffix = test_func.__name__
+    
+    
+    if prefix and suffix:
+        item._nodeid = f"{prefix} {suffix}"
+    elif prefix:
+        item._nodeid = prefix
+    elif suffix:
+        item._nodeid = suffix
